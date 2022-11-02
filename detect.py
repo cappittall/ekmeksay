b209fafa7f15aecf309984701751aceba48e1a4f
Hakan Çetin <netcat16@gmail.com>
2022-11-02 11:02:28 +0300

    qqq

--- detect.py
+++ detect.py
@@ -277,7 +277,7 @@ def render_gen(args):
                 roi_y = y0 * args.roi *.85
                 # roi_x = x0 * args.roi 
                 
-                objs =[obj for obj in objs if obj.bbox.ymin <= roi_y and obj.bbox.ymax >= roi_y*1.1 ]
+                objs =[obj for obj in objs if obj.bbox.ymin <= roi_y and obj.bbox.ymax >= roi_y ]
 
 
                 #objsss= len([obj for obj in objs if obj.bbox.ymax > roi_y * 0.95]) 
