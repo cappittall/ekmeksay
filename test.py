aeb13e4c3c0512f618a6683591b4588f7f96d93c
Hakan Çetin <netcat16@gmail.com>
2022-11-07 22:58:33 +0300

    updated

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ pat = '/home/mendel/ekmeksay/model/'
 version= str(max([int(i) for i in os.listdir(pat) if i.isdigit()]))
 model= pat+'modelv' + version + '_edgetpu.tflite'
 
+sayi=12345678910111213
+for i in range(2,sayi):
+    if sayi % (i) ==0:
+        print(i, 'Sayısına bölündü')
+
+        
