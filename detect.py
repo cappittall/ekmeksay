0b357c8d5419337add736bae35543ba2537ac1c4
Hakan Çetin <netcat16@gmail.com>
2022-12-14 10:54:16 +0300

    updated

--- detect.py
+++ detect.py
@@ -136,7 +136,7 @@ def overlay(layout, objs, trdata, axis, roi, inference_time, inference_rate, tre
 
         colorm = 'green' if labelid == 0.0 else 'yellow'
         # if labelid == Ekmek add to counter
-        if y+int(h)  > roi_y and y+int(h) < roi_y * 1.015 and trackID not in counted_ids and labelid == 0.0 and len(trdata)>3:
+        if y+int(h)  > roi_y and y+int(h) < roi_y * 1.015 and trackID not in counted_ids and labelid == 0.0 and len(trdata)>4:
             counter[4] += 1
             counter[3] += 1
             if counter[1] not in cesits: cesits[counter[1]] = 0
